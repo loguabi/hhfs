6b5cf9d9972c4cb9b3221cb8fa49fbde2fffa537
logitha <loga848.456@gmail.com>
2016-07-02 11:13:52 +0530

    Create hhfs.c

---
+++ hhfs.c
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Pj
+{
+public:
+calculate()
+{
+int a,b,c,k;
+cout<<"\n enter the 'a' for 2a";
+cin>>a;
+cout<<"\nenter the no of child pj design for the 2a-1 day";
+cin>.p;
+for(k=0;k<2*a;k++)
+{
+static int j=1;
+x=2*a-1;
+if(p!=0)
+{
+cout<<"\n the pj of stdudent no\t"<< c << \t"<<j++<<"\t"<<c"."<<b<<endl;}
+}
+}
+};
+int main()
+pj l;
+l.calculate();
+return 0;
+}
